68175c03639ffb21837d75269aedfc17d59f3887
Jan Tomalak <naj.tomalak@gmail.com>
2024-06-20 15:56:30 +0200

    Not existing command prompt

--- app/main.py
+++ app/main.py
@@ -6,12 +6,9 @@ def main():
     sys.stdout.flush()
 
     # Wait for user input
-    input()
+    command = input()
 
-    command_list = []
-    command = sys.argv[1]
-    if command not in command_list:
-        print(f"{command}: command not found")
+    sys.stdout(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
