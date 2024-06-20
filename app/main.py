e687be9abe91b825f2138c36b5c056afc4986e70
Jan Tomalak <naj.tomalak@gmail.com>
2024-06-20 22:23:58 +0200

    Reprograming type command

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 import sys
+import os
 
 
 def main():
     
     valid_commands = ["echo", "exit", "type"]
+    PATH = os.environ.get("PATH")
+
+    paths = PATH.split(":")
 
     while True:
         sys.stdout.write("$ ")
@@ -21,11 +25,14 @@ def main():
                 output = " ".join(lst_input[1:])
                 print(output)
             elif lst_input[0] == "type":
-                if lst_input[1] == "ls":
-                    print(sys.argv[0])
-                    continue
-                elif lst_input[1] in valid_commands:
+                if lst_input[1] in valid_commands:
                     print(f"{lst_input[1]} is a shell builtin")
+                    continue
+                elif lst_input[1] == "ls":
+                    print(f"ls is {paths[0]}/ls")
+                    continue
+                elif lst_input[1] in paths[1]:
+                    print(f"{lst_input[1]} is {paths[1]}/{lst_input[1]}")
                     continue
                 else:
                     print(f"{lst_input[1]}: not found")
