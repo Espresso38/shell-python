abe65524ef49df70446030df53ff1c2ef3abcd12
Jan Tomalak <naj.tomalak@gmail.com>
2024-06-20 16:09:37 +0200

    Adding exit 0 event

--- app/main.py
+++ app/main.py
@@ -9,12 +9,13 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command = input()
-        if command not in valid_commands:
-            print(f"{command}: command not found")
-            continue
-        elif command == "exit 0":
+        if command == "exit 0":
             False
             sys.exit()
+        elif command not in valid_commands:
+            print(f"{command}: command not found")
+            continue
+        
 
 if __name__ == "__main__":
     main()
