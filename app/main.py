4aeee4418767413ae303b00f4519b1f1cda08260
Jan Tomalak <naj.tomalak@gmail.com>
2024-06-20 16:23:46 +0200

    Adding echo command

--- app/main.py
+++ app/main.py
@@ -9,16 +9,16 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command = input()
-        if command == "exit 0":
+        lst_input = command.split()
+        if lst_input[0] == "exit":
             False
             sys.exit()
-        elif command not in valid_commands:
+        elif lst_input[0] not in valid_commands:
             print(f"{command}: command not found")
             continue
         else:
-            if "echo" in command:
-                list_of_text = command.split()
-                output = " ".join(list_of_text[1:])
+            if lst_input[0] == "echo":
+                output = " ".join(lst_input[1:])
                 print(output)
         
 
