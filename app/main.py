5b26470a4b1e419efc8919fddf73ce7aea7ba067
Jan Tomalak <naj.tomalak@gmail.com>
2024-06-20 22:57:25 +0200

    Running files

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command = input()
+        cmd = command.split(" ")[1]
         lst_input = command.split()
         if lst_input[0] == "exit":
             False
@@ -24,14 +25,12 @@ def main():
             if lst_input[0] == "echo":
                 output = " ".join(lst_input[1:])
                 print(output)
-            if command.startswith("type"):
-                cmd = command.split(" ")[1]
-                cmd_path = None
-                paths = PATH.split(":")
-                if os.paths.isfile(cmd):
+            if os.path.isfile(command.split(" ")[0]):
                     os.system(command)
-                else:
-                    print(f"{command}: command not found")
+            else:
+                print(f"{command}: command not found")
+            if command.startswith("type"):
+                cmd_path = None
                 for path in paths:
                     if os.path.isfile(f"{path}/{cmd}"):
                         cmd_path = f"{path}/{cmd}"
